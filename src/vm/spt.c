f87c3255ffb26bdc34267edc3112b17e0148d22c
Kelsey Morris <klm3952@cs.utexas.edu>
2015-04-13 12:06:51 -0500

    Update spt.c

--- src/vm/spt.c
+++ src/vm/spt.c
@@ -6,15 +6,16 @@ struct sptEntry{
 	bool isStackPage;
 	bool isSwapped;
 	bool inFileSys;
-	void *addr;
+	void *vaddr;
 	struct hash_elem hash_elem;
 }
 
-updateSPT(){
+updateSPT(void *vaddr){
 	struct thread *currThread = thread_current();
-	bool uninitialized = currThread->uninitialized; //ADD TO THREAD STRUCT
+	bool uninitialized = currThread->sptUninitialized; //ADD TO THREAD STRUCT
 	if (uninitialized){
 		setUp();
+		currThread->sptUninitialized = 0;
 	}
 	update();
 }
@@ -24,11 +25,15 @@ setUp(){
 	struct thread *currThread = thread_current();
 	struct hash *threadSPT;
 	hash_init(threadSPT, &hashFunction, &lessFunction, NULL);
-	currThread->spt = hash;
+	currThread->spt = threadSpt;
 }
 
 update(){
 
+}
+
+void *resourceToFree(){
+	return NULL;
 }
 
 // Define a hash function for this data type:
@@ -36,7 +41,7 @@ update(){
 // with PintOS's int hash function.
 unsigned hashFunction(const struct hash_elem *e, void *aux){
 	struct sptEntry *spt = hash_entry(e, struct sptEntry, hash_elem);
-	return hash_int(spt->addr);
+	return hash_int(spt->vaddr);
 }
 
 // Define a less function to initilize the hash table.
@@ -44,5 +49,5 @@ unsigned hashFunction(const struct hash_elem *e, void *aux){
 bool lessFunction(const struct hash_elem *one, const struct hash_elem *two, void *aux){
 	struct sptEntry *entryOne = hash_entry(one, struct sptEntry, hash_elem);
 	struct sptEntry *entryTwo = hash_entry(two, struct sptEntry, hash_elem);
-	return (entryOne->addr) < (entryTwo->addr);
+	return (entryOne->vaddr) < (entryTwo->vaddr);
 }

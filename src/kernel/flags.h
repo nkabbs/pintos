c9555155b9d1ec176917042d92214b41b0b6e9a1
Nicholas Abbs <nkabbs@cs.utexas.edu>
2015-04-15 16:57:52 -0500

    Removed extraneous files

--- src/kernel/flags.h
+++
@@ -1,8 +0,0 @@
-#ifndef KERNEL_FLAGS_H
-#define KERNEL_FLAGS_H
-
-/* EFLAGS Register. */
-#define FLAG_MBS  0x00000002    /* Must be set. */
-#define FLAG_IF   0x00000200    /* Interrupt Flag. */
-
-#endif /* kernel/flags.h */
--- src/kernel/syscall.h
+++
@@ -1,7 +0,0 @@
-#ifndef KERNEL_SYSCALL_H
-#define KERNEL_SYSCALL_H
-
-void syscall_init (void);
-void syscall_exit (void);
-
-#endif /* kernel/syscall.h */
